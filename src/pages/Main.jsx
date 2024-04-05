2f1968c7ba9695685d3f75862a334b924de288c3
elias11mendez <mendezeliasjosue58@gmail.com>
2024-04-05 16:25:56 -0600

    se coloca el icono correspondiente a la web

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,7 +7,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 const Main = () => {
   return (
     <>
-      <Link to={'https://github.com/elias11mendez'}> 
+      <Link to={'https://github.com/elias11mendez'} target="_blank"> 
         <FontAwesomeIcon
           className="ml-auto pb-7 h-8"
           icon={faGithub}
