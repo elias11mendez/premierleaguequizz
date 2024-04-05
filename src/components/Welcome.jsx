794f8915721657f1498e497b556e5fd05a70b2b4
elias11mendez <mendezeliasjosue58@gmail.com>
2024-04-05 15:57:10 -0600

    se agrega enlace a github

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
 import {  useNavigate } from "react-router-dom";
 const Welcome = () => {
   const navigate = useNavigate();
@@ -9,7 +6,6 @@ const Welcome = () => {
   };
   return (
     <div className="flex justify-center items-center flex-col m-2 ">
-<FontAwesomeIcon href='https://gi thub.com/elias11mendez' className='ml-auto pb-7 h-8' icon={faGithub} />
       <h1 className="text-6xl font-bold text-center text flex justify-center">
         BIENVENIDO
       </h1>
@@ -24,13 +20,8 @@ const Welcome = () => {
         >
           INICIAR
         </button>
-
-
-      </div>
-      
+      </div>      
       <img className="bg-white w-52 h-52 rounded-full" src="https://dorve.com/wp-content/uploads/2023/08/overlap-premier-league-logo.png"/>
-            {/* <FontAwesomeIcon className='ml-auto pb-7 h-8' icon={faGithub} /> */}
-
     </div>
   );
 };
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,21 @@
-import Welcome from "../components/Welcome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
-const Main = ()=>{
-    return(
-        <>
-        <Welcome/>
-        </>
-    )
-}
-export default Main
+import Welcome from "../components/Welcome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const Main = () => {
+  return (
+    <>
+      <Link to={'https://github.com/elias11mendez'}> 
+        <FontAwesomeIcon
+          className="ml-auto pb-7 h-8"
+          icon={faGithub}
+        />
+      </Link>
+
+      <Welcome />
+    </>
+  );
+};
+export default Main;
